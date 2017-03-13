6659cd797ce123c93b6402f6512042bbad4d6815
Miguel Andrade <jmandrade0@gmail.com>
2017-03-13 19:44:59 +0000

    update package.json details

--- tests/integration/components/render-template-test.js
+++ tests/integration/components/render-template-test.js
@@ -5,21 +5,43 @@ moduleForComponent('render-template', 'Integration | Component | render template
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{render-template}}`);
-
-  assert.equal(this.$().text().trim(), '');
+test('it renders bare text', function(assert) {
+  assert.expect(1);
 
   // Template block usage:
   this.render(hbs`
-    {{#render-template}}
-      template block text
-    {{/render-template}}
+    {{render-template templateString="abc"}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'abc');
+});
+
+test('it renders bound values', function(assert) {
+  assert.expect(2);
+
+  this.set('a', 1);
+
+  // Template block usage:
+  this.render(hbs`
+    {{render-template templateString="{{a}}" props=(hash a=a)}}
+  `);
+
+  assert.equal(this.$().text().trim(), '1');
+
+  this.set('a', 2);
+
+  assert.equal(this.$().text().trim(), '2');
+});
+
+test('it renders helpers', function(assert) {
+  assert.expect(1);
+
+  this.set('a', 1);
+
+  // Template block usage:
+  this.render(hbs`
+    {{render-template templateString="{{concat a 2}}" props=(hash a=a)}}
+  `);
+
+  assert.equal(this.$().text().trim(), '12');
 });
